3033e31b691d945b6fbfd79ab98c9a3df4cea09e
Maximus Brutalis <dmitroid1sk@gmail.com>
2024-07-27 21:05:06 -0500

    03, 04 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string
+        .split('')  // Convert string to an array of characters
+        .reverse()  // Reverse the array
+        .join('');  // Join the array back into a string
 };
 
 // Do not edit below this line
